59c596a0eb48e415076d6646e82754927eb3a4b3
Robert Moya <Robert.moya92@yahoo.com>
2019-10-12 01:38:13 -0700

    trying different methods

--- test.js
+++ test.js
@@ -4,12 +4,14 @@ var button1 = document.getElementById("Linkedin");
 var button2 = document.getElementById("Github");
 
 button.onclick= function (){
-    location.replace("google.com");  
+    document.getElementById("about me").innerHTML = location.replace("google.com");  
 };
 
 button1.onclick= function (){
+    document.getElementById("Linkedin").innerHTML=
     window.location.href=("https://www.linkedin.com/in/robert-moya/");
 };
 button2.onclick= function (){
+    document.getElementById("Github").innerHTML=
     window.location.href=("https://github.com/RobertMoya");
 };
