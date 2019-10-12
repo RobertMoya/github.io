6c695e6d848ae097aa464a56d9a3d8d1d69f8602
Robert Moya <Robert.moya92@yahoo.com>
2019-10-12 01:43:38 -0700

    testing

--- test.js
+++ test.js
@@ -4,14 +4,12 @@ var button1 = document.getElementById("Linkedin");
 var button2 = document.getElementById("Github");
 
 button.onclick= function (){
-    document.getElementById("about me").innerHTML = location.replace("google.com");  
+    document.getElementById("about me").innerHTML = window.location.href("google.com");  
 };
 
 button1.onclick= function (){
-    document.getElementById("Linkedin").innerHTML=
-    window.location.href=("https://www.linkedin.com/in/robert-moya/");
+    document.getElementById("Linkedin").innerHTML= window.location.href=("https://www.linkedin.com/in/robert-moya/");
 };
 button2.onclick= function (){
-    document.getElementById("Github").innerHTML=
-    window.location.href=("https://github.com/RobertMoya");
+    document.getElementById("Github").innerHTML = window.location.href=("https://github.com/RobertMoya");
 };
