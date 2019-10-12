c4ef56675f3c2b07f81a08fec29563a4fc286446
Robert Moya <Robert.moya92@yahoo.com>
2019-10-12 01:11:43 -0700

    chosing the location on new buttons

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
 var content = document.getElementById("container");
 var button = document.getElementById("about me");
+var button1 = document.getElementById("Linkedin");
+var button2 = document.getElementById("Github");
 
 button.onclick= function (){
     
+};
+
+button1.onclick= function (){
+    location.replace("https://www.linkedin.com/in/robert-moya/");
+};
+button2.onclick= function (){
+    location.replace("https://github.com/RobertMoya");
 };
