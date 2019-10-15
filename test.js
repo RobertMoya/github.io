381a7de483e894ef9a608c30e0b213874e970a98
Robert Moya <Robert.moya92@yahoo.com>
2019-10-14 18:57:05 -0700

    alot of changes

--- test.js
+++ test.js
@@ -1,16 +1,21 @@
-console.log("js is working");
-var content = document.getElementById("container");
-document.getElementById("about me")=function(){myFunction()};
-var button2 = document.getElementById("Linkedin");
-var button3 = document.getElementById("Github");
+//console.log("js is working");
+//var content = document.getElementById("container");
+//document.getElementById("about me")=function(){myFunction()};
+//var button1 = document.getElementById("About Me");
+//var button2 = document.getElementById("Linkedin");
+//var button3 = document.getElementById("Github");
 
-function myFunction (){
-     window.location.href("google.com");  
-};
+//function myFunction (){
+//     window.location.href("google.com");  
+//};
 
-button2.onclick= function (){
-    document.getElementById("Linkedin").innerHTML= window.location.href=("https://www.linkedin.com/in/robert-moya/");
-};
-button3.onclick= function (){
-    document.getElementById("Github").innerHTML = window.location.href=("https://github.com/RobertMoya");
-};
+//button2.onclick= function (){
+  //  document.getElementById("Linkedin").innerHTML= window.location.href=("https://www.linkedin.com/in/robert-moya/");
+//};
+//button3.onclick= function (){
+  //  document.getElementById("Github").innerHTML = window.location.href=("https://github.com/RobertMoya");
+//};
+
+function aboutMeFunction(){
+    window.location.assign("www.google.com")
+}
